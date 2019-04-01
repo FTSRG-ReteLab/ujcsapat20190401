e3477aacf259cf06272f26b4cc8ad3e2d72d6cb5
Dániel Móna <mona.daniel97@gmail.com>
2019-04-01 17:11:59 +0200

    Remove unnecessary followSpeed call

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -59,8 +59,6 @@ public class TrainControllerImpl implements TrainController {
             Thread.currentThread().interrupt();
         }
 
-        followSpeed();
-
     }
 
     public void doubleSpeedLimit() {
